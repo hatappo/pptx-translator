5171bca26e2580fe21b0d57cc13388e8687fecdd
hata <hatappo44@gmail.com>
2019-10-17 02:09:06 +0900

    Add generated files by lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ppt-tr
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ppt_tr/core_test.clj
@@ -0,0 +1,7 @@
+(ns ppt-tr.core-test
+  (:require [clojure.test :refer :all]
+            [ppt-tr.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
